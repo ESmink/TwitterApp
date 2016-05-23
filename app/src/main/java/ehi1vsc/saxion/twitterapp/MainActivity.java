f1ff4a02a87a26050f36e56732250f746f2fdac9
Gijs97 <gijs.abbring@live.nl>
2016-05-23 13:42:55 +0200

    twitter

--- app/src/main/java/ehi1vsc/saxion/twitterapp/MainActivity.java
+++ app/src/main/java/ehi1vsc/saxion/twitterapp/MainActivity.java
@@ -2,6 +2,8 @@ package ehi1vsc.saxion.twitterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.ListView;
 
 import org.json.JSONObject;
@@ -17,5 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listview = (ListView)findViewById(R.id.listView);
         listview.setAdapter(new TweetAdapter(getBaseContext(), Model.getInstance().getTweets()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
