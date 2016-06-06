6bb626af25b66dbe33385f85ebc7391b33a90142
Wrdmnr <wrdmnr@gmail.com>
2016-06-06 13:43:28 +0200

    Imported scribeJava!

--- app/src/main/java/ehi1vsc/saxion/twitterapp/Oauth/Request_Token.java
+++ app/src/main/java/ehi1vsc/saxion/twitterapp/Oauth/Request_Token.java
@@ -1,22 +1,14 @@
 package ehi1vsc.saxion.twitterapp.Oauth;
 
-import java.util.ArrayList;
-
 /**
  * Created by edwin_000 on 23/05/2016.
  */
 public class Request_Token extends TwitterOauth {
 
     public Request_Token(){
-        String password = "b5NRVMTKd9Y6JXjRlIAOGBnkG";
+        super("https://api.twitter.com/oauth/request_token", "POST");
+        properties.add(new String[]{"oauth_callback", "twitterclient://callback"});
 
-        ArrayList objects = new ArrayList();
-        objects.add("https://api.twitter.com/oauth/request_token");
-        objects.add("POST");
-        ArrayList<String[]> properties = new ArrayList<>();
-        properties.add(new String[]{"oauth_callback", "https://www.google.nl/"});
-        objects.add(properties);
-
-        super.execute(objects.toArray());
+        super.execute();
     }
 }
--- app/src/main/java/ehi1vsc/saxion/twitterapp/Oauth/TwitterOauth.java
+++ app/src/main/java/ehi1vsc/saxion/twitterapp/Oauth/TwitterOauth.java
@@ -1,12 +1,14 @@
 package ehi1vsc.saxion.twitterapp.Oauth;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -15,6 +17,15 @@ import javax.net.ssl.HttpsURLConnection;
  * Created by edwin_000 on 23/05/2016.
  */
 public abstract class TwitterOauth extends AsyncTask {
+    private String stringUrl;
+    private String requestMethod;
+    ArrayList<String[]> properties = new ArrayList<>();
+
+    public TwitterOauth(String stringUrl, String requestMethod) {
+        this.stringUrl = stringUrl;
+        this.requestMethod = requestMethod;
+    }
+
     /**
      * params:
      * 0 = String url
@@ -25,14 +36,16 @@ public abstract class TwitterOauth extends AsyncTask {
     @Override
     protected Object doInBackground(Object[] params) {
         try {
-            HttpsURLConnection con = (HttpsURLConnection) (new URL((String) params[0])).openConnection();
-            con.setRequestMethod((String) params[1]);
-            for (String[] keyValue : (List<String[]>) params[2]) {
+            //setting the header
+            HttpsURLConnection con = (HttpsURLConnection) (new URL(stringUrl)).openConnection();
+            con.setRequestMethod(requestMethod);
+            for (String[] keyValue : properties) {
                 con.setRequestProperty(keyValue[0], keyValue[1]);
             }
 
-            int responsecode = con.getResponseCode();
+            Log.d("Response:", con.getResponseCode() + "");
 
+            //reading response
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(con.getInputStream()));
             String inputLine;
